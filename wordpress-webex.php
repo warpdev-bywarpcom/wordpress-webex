84c99d5251885c31980806465fc1102e9275ed7f
NBajanca <nuno.bajanca@warpcom.com>
2021-02-05 16:31:34 +0000

    setup with wp scaffold

---
+++ wordpress-webex.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wordpress Webex
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wordpress-webex
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wordpress_Webex
+ */
+
+// Your code starts here.
